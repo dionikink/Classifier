98c15976d1938ef73eaccbf72c42792d27484fdc
Jens2 <j.vdmeer10@hotmail.com>
2015-11-20 12:15:30 +0100

    hier staat helemaal niets

--- src/main/java/dijons/classifier/Test.java
+++ src/main/java/dijons/classifier/Test.java
@@ -6,5 +6,6 @@ package dijons.classifier;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Welkom Jens!");
+        System.out.println("hoi");
     }
 }

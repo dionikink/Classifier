987b06955e995b1320e98f3878f11b669283065c
Dion Ikink <ikinkdion@gmail.com>
2015-11-20 11:45:50 +0100

    Welkom Jens!

---
+++ src/main/java/dijons/classifier/Test.java
@@ -0,0 +1,10 @@
+package dijons.classifier;
+
+/**
+ * Created by Dion Ikink on 20-11-2015.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Welkom Jens!");
+    }
+}
